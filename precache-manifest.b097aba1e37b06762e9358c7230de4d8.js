9801ca02539bd97d97530fb6dd065c89ee86c6c4
asudbury <sudburya@gmail.com>
2021-06-06 17:58:15 +0000

    Deploying to gh-pages from  @ 149e164f0eb3b1bcd2864a1298572a7a45473c25 🚀

---
+++ precache-manifest.b097aba1e37b06762e9358c7230de4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c931bc4156d87990e3e6c6e11109a36",
+    "url": "/react-gmail-clone/index.html"
+  },
+  {
+    "revision": "32d1607d124dcc739b11",
+    "url": "/react-gmail-clone/static/js/2.5f8eb30a.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/react-gmail-clone/static/js/2.5f8eb30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f48eb9cbfb7d5ff90f",
+    "url": "/react-gmail-clone/static/js/main.8b2c32a3.chunk.js"
+  },
+  {
+    "revision": "9401b733d3bc09ee9b76",
+    "url": "/react-gmail-clone/static/js/runtime-main.ce3205af.js"
+  }
+]);
